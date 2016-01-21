ebf27f15e5c02532794e3dbe70ba475224e3ac24
Miki Hardisty <mhardist@eng.ucsd.edu>
2016-01-21 15:06:16 -0800

    checking in

--- 201CatsDB_Create.sql
+++ 201CatsDB_Create.sql
@@ -1,16 +1,35 @@
-﻿CREATE TABLE users (
+﻿-- I could opt to use the fb_login ONLY as the PRIMARY KEY and the code for the 
+-- scope of the project would be fine and probably less redundant.   That just 
+-- "feels" wrong.  I choose to use a serial primary key instead. Here's one use-case - 
+-- in the future, facebook may not be the only social site that I want to track
+-- friends and followers of friends.  What if I wanted to use instagram, twitter, etc
+-- social media outlets?  I may want to offer recommendations from friends of
+-- friends on instagram as well as facebook.  So, I know this isn't in the homework
+-- requirements, but this is why I designed it as such....
+CREATE TABLE users (
 	id			SERIAL PRIMARY KEY,
-	user_name		TEXT UNIQUE,
-	fb_login		TEXT UNIQUE
+	user_name		TEXT NOT NULL UNIQUE,
+	fb_login		TEXT NOT NULL UNIQUE
 );
 
+-- For this table, I thought that you really have to have a url to display the video,
+-- but also thought that a url wasn't going to be readable by the user, and that a title
+-- was needed instead.  I included both.  I considered putting NOT NULL contraints on 
+-- each the title and the url, but I thought of a case where I could have a null
+-- title, because all I really need to show the video is the url.
+-- Regardless of whether or not I need a title or url, I know that the title and url 
+-- together should be unique. With them together being unique, I could set it as a primary
+-- key, but that would impact performance of queries.
 CREATE TABLE video (
 	id			SERIAL PRIMARY KEY,
 	title			TEXT,
-	url			TEXT,
+	url			TEXT NOT NULL,
 	UNIQUE (title, url)
 );
 
+-- The UNIQUE constraint restricts users from liking a video more than once.
+-- The NOT NULL for user_id means that in order for the video to be liked
+-- the user must be known, and thus have a user_id.
 CREATE TABLE like_activity (
 	liked_datetime		TIMESTAMP WITHOUT TIME ZONE NOT NULL,
 	user_id			INTEGER REFERENCES users (id) NOT NULL,
@@ -24,18 +43,39 @@ CREATE TABLE watch_activity (
 	video_id		INTEGER REFERENCES video (id) NOT NULL
 );
 
+-- When a user successfully logs in, that user creates a new session.  This
+-- table is named sessions because it is the result of a successful login.
+-- I will probably want to track more things in the sessions table like the GUID, 
+-- the device the user logged in with, the geolocation info, browser info, etc.
+-- Although this wasn’t in the homework assignment definition, I chose to separate
+-- this table from the suggested_content table so that this info could also
+-- potentially be stored.
+
 CREATE TABLE sessions (
 	id			SERIAL PRIMARY KEY,
 	start_time		TIMESTAMP WITHOUT TIME ZONE NOT NULL,
 	user_id			INTEGER REFERENCES users (id) NOT NULL
 );
 
+-- I’m choosing not to put a constraint of 10 videos per session on this table.
+-- Yes, this may result in a varying number of videos for the session, but this
+-- logic is best placed in the business layer.  If the business direction was 
+-- to change, we don’t want to have to change the schema, we want to make 
+-- modifications in the business logic layer.  If I were to be EXACT, I would 
+-- architect the table using session_id, video1, video2, video3, etc.  That’s 
+-- ugly and not flexible.
 
 CREATE TABLE suggested_content (
 	session_id		INTEGER REFERENCES sessions (id) NOT NULL,
 	video_id		INTEGER REFERENCES video (id) NOT NULL
 );
 
+
+-- Since the homework instructions state that there is some leeway on capturing 
+-- redundancy, I made the table symmetric and am going to handle the redundancy
+-- in my queries.
+-- This table would be the result of mapping the FB friends information to the 
+-- users table. 
 CREATE TABLE friends (
 	subject			INTEGER REFERENCES users (id) NOT NULL,
 	object			INTEGER REFERENCES users (id) NOT NULL,
--- option_queries.sql
+++ option_queries.sql
@@ -1,7 +1,4 @@
-﻿insert into users (user_name, fb_login) values ('claire','cbhardisty');
-
-
--- OVERALL LIKES
+﻿-- OVERALL LIKES
 -- Option “Overall Likes”: The Top-10 cat videos are the ones that have collected the highest
 -- numbers of likes, overall.
 select v.title, v.url, count(video_id) as like_count 
@@ -13,26 +10,97 @@ limit 10
 -- FRIEND LIKES
 -- Option “Friend Likes”: The Top-10 cat videos are the ones that have collected the highest 
 -- numbers of likes from the friends of X.
--- friends of X
--- subject == 1 is X
-select v.title, v.url, count(la.video_id) as video_like_count
-from like_activity la join video v on la.video_id = v.id
-where user_id in ( select u.id
-			from friends f join users u on f.object = u.id
-			where subject = 1)
-group by v.title, v.url
-order by video_like_count desc
-limit 10
+-- Friends of X also means that X's likes are not included
+SELECT v.title, v.url, count(la.video_id) as video_like_count
+FROM video v JOIN like_activity la ON v.id = la.video_id
+	JOIN users u ON u.id = la.user_id 
+	JOIN friends f ON u.id = f.object AND la.user_id != f.subject
+WHERE f.subject = 1
+GROUP BY v.title, v.url
+ORDER BY video_like_count DESC
+LIMIT 10;
 
---Option “Friends-of-Friends Likes”: The Top-10 cat videos are the ones that have collected the highest 
+-- Option “Friends-of-Friends Likes”: The Top-10 cat videos are the ones that have collected the highest 
 -- numbers of likes from friends and friends-of-friends.
--- first get friends of friends
-select u.id
-from friends f join users u on f.object = u.id
-where subject = 1
+-- I'm assuming that even though it is possible for X to be a friend of a friend of him/herself, this is not 
+-- the intent.  Intent should be that we get friends of friends that aren't X.
 
+-- Using a subquery
+SELECT v.title, count(la.video_id) as video_like_count
+FROM like_activity la JOIN video v ON la.video_id = v.id
+WHERE la.user_id IN (
+	SELECT f1.object
+	FROM friends f1 JOIN friends f2 ON f1.object = f2.subject
+	WHERE f1.subject = 1
+	GROUP BY f1.object)
+AND la.user_id != 1
+GROUP BY v.title, v.url, la.video_id
+ORDER BY video_like_count DESC
+LIMIT 10;
+
+-- using count distinct with no subquery
+SELECT v.title, count(distinct la.user_id) my_count
+FROM friends f1 JOIN friends f2 ON f1.object = f2.subject 
+	JOIN like_activity la ON la.user_id = f1.object AND la.user_id != f1.subject
+	JOIN video v ON la.video_id = v.id
+WHERE f1.subject = 1
+GROUP BY v.title
+ORDER BY my_count desc
+LIMIT 10;
 
 --Option “My kind of cats”: The Top-10 cat videos are the ones that have collected 
 -- the most likes from users who have liked at least one cat video that was liked by X.
---Option “My kind of cats – with preference (to cat aficionados that have the same tastes)”: The Top-10 cat videos are the ones that have collected the highest sum of weighted likes from every other user Y (i.e., given a cat video, each like on it, is multiplied according to a weight).The weight is the log cosine lc(X,Y) defined as follows: Conceptually, there is a vector vx for each user Y, including the logged-in user
---X. The vector has as many elements as the number of cat videos. Element i is 1 if Y liked the ith cat video; it is 0 otherwise. For example, if 201Cats has five cat videos and user 21 liked only the 1st and the 4th, the v21=<1,0,0,1,0>, i.e., v21[1]=v21[4]=1 and v21[2]= v21[3]=v21[5]=0. Assuming there are N cat videos, the log cosine lc(X, Y) is
+-- In this case, to me, this seems to answer the question of people like you also
+-- liked these other videos.  Again, it doesn't make sense to include the likes from X
+-- if we are trying to determine what other people like X liked.
+-- Breaking it down:
+-- Videos that were liked by X
+SELECT video_id
+FROM like_activity
+WHERE user_id = 1
+-- users who have liked at least one cat video liked by X
+SELECT DISTINCT user_id
+FROM like_activity
+WHERE video_id IN (SELECT video_id
+		FROM like_activity
+		WHERE user_id = 1)
+AND user_id != 1
+ORDER BY user_id
+-- now without subquery
+SELECT DISTINCT la2.user_id
+FROM like_activity la1 
+	JOIN like_activity la2 ON la1.video_id = la2.video_id AND la1.user_id != la2.user_id
+WHERE la1.user_id = 1
+ORDER BY la2.user_id
+
+-- now top videos that have collected the most likes from users who like at least one cat video liked by X
+-- using subquery
+SELECT title, count(distinct user_id)
+FROM like_activity la JOIN video v ON la.video_id = v.id
+WHERE user_id in (SELECT DISTINCT la2.user_id
+		FROM like_activity la1 JOIN like_activity la2 ON la1.video_id = la2.video_id AND la1.user_id != la2.user_id
+		WHERE la1.user_id = 1)
+GROUP BY title
+ORDER BY count(title) desc
+LIMIT 10
+
+-- without subquery
+SELECT v.title, count(distinct la3.user_id)
+FROM like_activity la1 JOIN like_activity la2 ON la1.video_id = la2.video_id AND la1.user_id != la2.user_id
+	JOIN like_activity la3 ON la2.user_id = la3.user_id
+	JOIN video v ON la3.video_id = v.id
+WHERE la1.user_id = 1
+GROUP BY v.title
+ORDER BY count(distinct la3.user_id) DESC
+
+
+-- Option “My kind of cats – with preference (to cat aficionados that have the same tastes)”: 
+-- The Top-10 cat videos are the ones that have collected the highest sum of weighted likes 
+-- from every other user Y (i.e., given a cat video, each like on it, is multiplied according to a weight).
+-- The weight is the log cosine lc(X,Y) defined as follows: Conceptually, there is a 
+-- vector vx for each user Y, including the logged-in user
+--X. The vector has as many elements as the number of cat videos. Element i is 1 if Y liked the ith cat 
+-- video; it is 0 otherwise. For example, if 201Cats has five cat videos and user 21 liked only the 1st and the 4th, 
+-- the v21=<1,0,0,1,0>, i.e., v21[1]=v21[4]=1 and v21[2]= v21[3]=v21[5]=0. Assuming there are N cat videos, the log cosine lc(X, Y) is
+
+
