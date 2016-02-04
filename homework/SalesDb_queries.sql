863548f8ea8f90a86556f3795da8687f175ece61
Miki Hardisty <mhardist@eng.ucsd.edu>
2016-02-04 11:45:53 -0800

    upload

--- homework/SalesDb_queries.sql
+++ homework/SalesDb_queries.sql
@@ -57,9 +57,9 @@ ORDER BY total_paid DESC
 LIMIT 20
 
 -- Each combination of top 20 categories and top 20 customers
-SELECT p.product_name, c.customer_name 
-FROM product p, customer c
-WHERE p.product_id IN (SELECT cat.category_id
+SELECT cat1.category_id, c.customer_id
+FROM category cat1, customer c
+WHERE cat1.category_id IN (SELECT cat.category_id
 			FROM sale s JOIN product p ON s.product_id = p.product_id
 				JOIN category cat ON cat.category_id = p.category_id
 			GROUP BY cat.category_id
@@ -71,13 +71,17 @@ AND c.customer_id IN (SELECT c.customer_id
 			ORDER BY sum(s.price) DESC
 			LIMIT 20)
 
--- And the final query and resulting tuple:
-SELECT p.product_name, c.customer_name, COALESCE(sum(quantity),0) as total_qty, COALESCE(sum(price),0) as total_paid
-FROM sale s RIGHT JOIN (SELECT p.product_id, c.customer_id 
-			FROM product p, customer c
-			WHERE p.product_id IN (SELECT cat.category_id
-						FROM sale s JOIN product p ON s.product_id = p.product_id
-							JOIN category cat ON cat.category_id = p.category_id
+-- And the semi-final query and resulting tuple, although this one doesn't quite get ALL top customer and ALL top products
+-- even if the sum of the total paid is 0
+SELECT cat.category_name, c.customer_name, sum(quantity) as total_qty, sum(price) as total_paid
+FROM sale s JOIN product p ON s.product_id = p.product_id
+	    JOIN customer c ON s.customer_id = c.customer_id
+	    RIGHT JOIN (SELECT cat1.category_id, c.customer_id
+			FROM category cat1, customer c
+			WHERE cat1.category_id IN (SELECT cat.category_id
+						FROM sale s 
+						JOIN product p ON s.product_id = p.product_id
+						JOIN category cat ON cat.category_id = p.category_id
 						GROUP BY cat.category_id
 						ORDER BY sum(s.price) DESC
 						LIMIT 20)
@@ -86,8 +90,41 @@ FROM sale s RIGHT JOIN (SELECT p.product_id, c.customer_id
 						GROUP BY c.customer_id
 						ORDER BY sum(s.price) DESC
 						LIMIT 20)) a 
-			ON s.product_id = a.product_id AND s.customer_id = a.customer_id
-	JOIN customer c ON c.customer_id = a.customer_id
-	JOIN product p ON p.product_id = a.product_id
-GROUP BY p.product_name, c.customer_name
-ORDER BY p.product_name, c.customer_name
+			ON p.category_id = a.category_id
+			AND c.customer_id = a.customer_id
+	     JOIN category cat ON cat.category_id = a.category_id
+GROUP BY category_name, c.customer_name
+ORDER BY c.customer_name
+
+
+-- This query shows the top 20 customers and the top 20 product categories along side their total sales and total qty
+-- and includes ALL 20 customers in ALL 20 product categories regardless if their total sales in one of the top
+-- categories is 0.
+SELECT cat.category_name, top.category_id, c.customer_name, top.customer_id, COALESCE(price,0) total_price, COALESCE(quantity,0) total_qty
+FROM (SELECT category_id, customer_id 
+		FROM (SELECT cat.category_id
+			FROM category cat, product p, sale s
+			WHERE s.product_id = p.product_id
+			AND cat.category_id = p.category_id
+			GROUP BY cat.category_id
+			ORDER BY sum(s.price) DESC
+			LIMIT 20) cat1, 
+			(SELECT customer_id
+			FROM sale s
+			GROUP BY customer_id
+			ORDER BY sum(s.price) DESC
+			LIMIT 20
+			) c1
+	) top LEFT OUTER JOIN 
+	(SELECT sum(s.price) price, sum(s.quantity) quantity, p.category_id, s.customer_id
+		FROM sale s, product p
+		WHERE s.product_id = p.product_id
+		GROUP BY p.category_id, s.customer_id
+	) merge
+	ON  top.customer_id = merge.customer_id
+	AND top.category_id = merge.category_id 
+JOIN customer c ON top.customer_id = c.customer_id
+JOIN category cat ON top.category_id = cat.category_id 
+JOIN product p ON p.category_id = cat.category_id
+GROUP BY cat.category_name, top.category_id, c.customer_name, top.customer_id, price, quantity
+ORDER BY cat.category_name, c.customer_name
